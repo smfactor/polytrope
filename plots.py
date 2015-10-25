a222a600b75dd99d485d025d2d6b60782d21ddbf
Samuel Factor <sfxfactor@gmail.com>
2015-10-24 19:45:45 -0500

    finished plots and grav pot. need central temp

--- plots.py
+++ plots.py
@@ -2,13 +2,59 @@ import numpy as np
 import matplotlib.pyplot as plt
 import polytrope as poly
 import scipy.interpolate as intp
+from astropy.io import fits
+import sys
 
-data = poly.poly(2.9,0.001,0.00001,'poly2p9')
+n = float(sys.argv[1])
+filename='poly'+str(n)
+try:
+    data = fits.getdata(filename+'.fits')
+except IOError:
+    data = poly.poly(n,0.0001,0.000001,filename)
 
 z=data[0]
 th=data[1]
 mz2dthdz=data[2]
 m3ozdthdz=data[3]
 
-z2p9 = intp.interp1d(z[-2:-1],th[-2:-1])(0.)
-print z2p9
+zn = intp.UnivariateSpline(th[-2:][::-1],z[-2:][::-1],k=1)(0.)
+mz2dthdzn = intp.UnivariateSpline(z[-2:],mz2dthdz[-2:],k=1)(zn)
+print "zn = ", zn
+print "(-z^2 dth/dz)zn = ",mz2dthdzn
+
+Msun = 1.989e33 #g
+Rsun = 6.9599e10 #cm
+G = 6.67259e-8 #cm^3 g^-1 s^-2
+
+M = 200. * Msun
+R = 7. * Rsun
+
+pc = (M/(4.*np.pi*mz2dthdzn))*(zn/R)**3.
+print "pc = ",pc
+A = zn/R
+print "A = ",A
+K=((4.*np.pi*G)/(n*A**2.))*pc**(1.-1./n)
+print "K = ",K
+Pc=K*pc**(1.+1./n)
+print "Pc = ",Pc
+#Tc = 
+
+r = z/A
+p = pc*th**n
+P = K*p**(1.+1./n)
+plt.plot(r,p)
+plt.savefig('density.pdf')
+plt.clf()
+plt.plot(r,P)
+plt.savefig('pressure.pdf')
+plt.clf()
+
+m = (4./3.)*np.pi*p*r**3.
+plt.plot(r,m)
+plt.savefig('mass.pdf')
+plt.clf()
+
+U = -G*4.*np.pi* np.trapz((m*r*p),r)
+print U
+print -G*(16./15.)*np.pi**2*(pc*m3ozdthdz[-1])**2*R**5
+
--- polytrope.py
+++ polytrope.py
@@ -4,31 +4,32 @@ import matplotlib.pyplot as plt
 from astropy.io import fits
 import sys
 
-def LEeqn(th,z,n):
-    return np.array([th[1],(-2./z)*th[1]-(th[0]**n)])
+def LEeqn(n):
+    def LEeq(th,z):
+        return np.array([th[1],(-2./z)*th[1]-(th[0]**n)])
+    return LEeq
 
 def thPN(th,z):
     return np.array([(-1./3.)*z+(1./30.)*z**3, (-1./3.)+(1./10.)*z**2])
 
 def poly(n,zi,dz,filename):
+    LEeq = LEeqn(n)
     zs = np.arange(0.,zi,dz)
     nz = np.size(zs)
 
     y = intg.odeint(thPN,np.array([1.,0.]),zs)
 
     while y[-1,0]>0.:
-        nz = np.size(zs)
-        zs2 = np.arange(zs[-1],zs[-1]+100.*dz,dz)
+        zs2 = np.arange(zs[-1],zs[-1]+200.*dz,dz)
         zs = np.append(zs,zs2)
-        y = np.append(y,intg.odeint(LEeqn,y[nz-1],zs2,args=tuple([n])),axis=0)
+        y = np.append(y,intg.odeint(LEeq,y[-1],zs2),axis=0)
         sys.stdout.write("\rz={0},th={1}".format(zs[-1],y[-1,0]))
         sys.stdout.flush()
     sys.stdout.write("\n")
 
-    nover = np.size(np.where(y[:,0]<0.)-1
-    print nover
-    zs = zs[:-nover]
-    y = y[:-nover]
+    ngood= np.size(np.where(y[:,0]>0.))
+    zs = zs[:ngood]
+    y = y[:ngood]
 
     data = np.array([zs,y[:,0],-(zs**2)*y[:,1],(-3./zs)*y[:,1]])
 
