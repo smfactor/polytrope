daf83dd9680c0c7ffd88b8678be95c69b9ab3653
Samuel Factor <sfactor@wesleyan.edu>
2015-11-01 21:43:12 -0600

    comment code

--- plots.py
+++ plots.py
@@ -5,23 +5,28 @@ import scipy.interpolate as intp
 from astropy.io import fits
 import sys
 
+#argument to plots.py is the index of the polytrope
 n = float(sys.argv[1])
 filename='poly'+str(n)
+#if the fits file exists, use it. If not, integrate to get the data
 try:
     data = fits.getdata(filename+'.fits')
 except IOError:
     data = poly.poly(n,0.0001,0.000001,filename)
 
+#split out important colums of data
 z=data[0]
 th=data[1]
 mz2dthdz=data[2]
 m3ozdthdz=data[3]
 
+#extrapolate using linear interpolation to 0
 zn = intp.UnivariateSpline(th[-2:][::-1],z[-2:][::-1],k=1)(0.)
 mz2dthdzn = intp.UnivariateSpline(z[-2:],mz2dthdz[-2:],k=1)(zn)
 print "zn = ", zn
 print "(-z^2 dth/dz)zn = ",mz2dthdzn
 
+#constants
 Msun = 1.989e33 #g
 Rsun = 6.9599e10 #cm
 G = 6.67259e-8 #cm^3 g^-1 s^-2
@@ -29,6 +34,7 @@ G = 6.67259e-8 #cm^3 g^-1 s^-2
 M = 200. * Msun
 R = 7. * Rsun
 
+#print important values
 pc = (M/(4.*np.pi*mz2dthdzn))*(zn/R)**3.
 print "pc = ",pc
 A = zn/R
@@ -37,11 +43,13 @@ K=((4.*np.pi*G)/(n*A**2.))*pc**(1.-1./n)
 print "K = ",K
 Pc=K*pc**(1.+1./n)
 print "Pc = ",Pc
-#Tc = 
 
+#replace . with p in plot filenames so LaTeX doesn't complain
 sn = list(str(n))
 sn[1]='p'
 sn="".join(sn)
+
+#set up dimentional variables and plot
 r = z/A
 p = pc*th**n
 P = K*p**(1.+1./n)
@@ -65,12 +73,15 @@ plt.clf()
 #plt.savefig('mass'+sn+'.pdf')
 #plt.clf()
 
+#constants
 a = 7.56591e-15
 B=0.5
+
+#temperature from eq 19.22
 Tc=(((1.-B)*3.*Pc)/a)**0.25
 print "Tc = ",Tc
 
 #U = -G*4.*np.pi* np.trapz((menc*p*r),r)
 #print "integrated Ugrav = ", U
-print "Eg ave density = ",-G*(16./15.)*np.pi**2*(pc*m3ozdthdz[-1])**2*R**5
 print "Eg (19.44) = ",(-3./(5.-n))*(G*M*M)/R
+print "Eg ave density = ",-G*(16./15.)*np.pi**2*(pc*m3ozdthdz[-1])**2*R**5
--- polytrope.py
+++ polytrope.py
@@ -5,33 +5,51 @@ from astropy.io import fits
 import sys
 
 def LEeqn(n):
+    '''
+    Lane-Emden equation.
+    Given an n this function returns a new function which is the L-E equation for that n. That function rReturns an array of Theta and Theta' as a function of Theta and z
+    '''
     def LEeq(th,z):
+        #use abs so dont get a complex number when th<0 and n is not an integer
+        #dont care about any values when th is negative anyway
         return np.array([th[1],(-2./z)*th[1]-(np.abs(th[0])**n)])
     return LEeq
 
 def thPN(th,z):
+    #3rd order taylor expansion of the L-E equation
     return np.array([(-1./3.)*z+(1./30.)*z**3, (-1./3.)+(1./10.)*z**2])
 
 def poly(n,zi,dz,filename):
+    '''
+    Integrates the L-E equation and returns and writes to a file the important values.
+    n is the index, zi is the interval to use the polynomial solution, dz is the step size, filename is the prefix for the output file (filename+.fits)
+    '''
+    #set up equation and variables
     LEeq = LEeqn(n)
     zs = np.arange(0.,zi,dz)
     nz = np.size(zs)
 
+    #integrate over initial interval using polynomial
     y = intg.odeint(thPN,np.array([1.,0.]),zs)
 
+    #integrate until th<0
     while y[-1,0]>0.:
         zs2 = np.arange(zs[-1],zs[-1]+200.*dz,dz)
         zs = np.append(zs,zs2)
         y = np.append(y,intg.odeint(LEeq,y[-1],zs2),axis=0)
+        #write progress
         sys.stdout.write("\rz={0},th={1}".format(zs[-1],y[-1,0]))
         sys.stdout.flush()
     sys.stdout.write("\n")
 
+    #only keep values where th>0
     ngood= np.size(np.where(y[:,0]>0.))
     zs = zs[:ngood]
     y = y[:ngood]
 
+    #prepare output array
     data = np.array([zs,y[:,0],-(zs**2)*y[:,1],(-3./zs)*y[:,1]])
 
+    #write and return data
     fits.writeto(filename+'.fits',data)
     return data
